dc0ea0a6476f4d926ec08a65e966e9ed195a97ca
Ludovic de Repentigny <ludovic.de-repentigny.1@ulaval.ca>
2021-02-08 17:44:51 -0500

    Modifications graphtemplate
    Ajout d'une méthode Save permettant de sauvegarder les graphiques. Optimisation de la génération des points lorsqu'il y a plusieurs courbes de données. Il est maintenant possible de modifier les ticks des axes dans la méthode Plot

--- graph_template.py
+++ graph_template.py
@@ -10,6 +10,7 @@ class GraphTemplate:
         self.Yunits = Yunits
         self.Yname = Yname
         self.plotlist = []
+        self.fig = None
     
     def AddData(self, data_x, data_y, label):
         if len(data_x) != len(data_y):
@@ -20,22 +21,37 @@ class GraphTemplate:
             raise TypeError("le label doit être une chaîne de caractères")
         self.plotlist.append((data_x,data_y, label))
     
-    def Plot(self):
+    def Plot(self, MajXTicks = None, MajYTicks = None, MinXTicks = None, MinYTicks =None):
+        Xmax = None
+        Ymax = None
+        fig,ax = plt.subplots()
         liste_styles = [("o", "k", "k"), ("o", "k", "w"), ("^", "k", "k"), ("^", "k", "w"), ("s", "k", "k"), ("s", "k", "w")]
         for ordre,data in enumerate(self.plotlist):
             plt.plot(data[0], data[1], liste_styles[ordre][0], color = liste_styles[ordre][1], mfc = liste_styles[ordre][2],label =f"{data[2]}")
-        plt.xlabel(f'{self.Xname} [{self.Xunits}]')
-        plt.ylabel(f'{self.Yname} [{self.Yunits}]')
-        plt.title(self.Title)
-        plt.legend(loc = "lower right")
-        plt.show()
+            if Xmax == None or max(data[0]) > Xmax:
+                Xmax = max(data[0])
+            if Ymax == None or max(data[1]) > Ymax:
+                Ymax = max(data[1])
+        plt.xlabel(f'{self.Xname} [{self.Xunits}]',fontsize=12)
+        plt.ylabel(f'{self.Yname} [{self.Yunits}]',fontsize=12)
+        plt.title(self.Title, fontweight = "bold",fontsize=12)
+        plt.legend(loc = "lower right",bbox_to_anchor=(1.45, 0.1),fancybox=True, shadow=True)
+        if MajXTicks != None:
+            plt.xticks(np.arange(0,Xmax,MajXTicks))
+        if MinXTicks != None:
+            ax.set_xticks(np.arange(0,Xmax + Xmax / 20, MinXTicks), minor=True)
+        if MajYTicks != None:
+            plt.yticks(np.arange(0,Ymax,MajYTicks))
+        if MinYTicks != None:
+            ax.set_yticks(np.arange(0,Ymax + Ymax / 20, MinYTicks), minor=True)
+        plt.show(fig)
+        self.fig = fig
+    
+    def Save(self, format = "png", dpival = 600):
+        if format == "png":
+            self.fig.savefig("Pockels.png",bbox_inches='tight',dpi=dpival)
+        if format == "pdf":
+            self.fig.savefig("Pockels.pdf",bbox_inches='tight')
 
-Test = GraphTemplate()
-Test.AddData([1,2,3,4,5],[1,2,3,4,5],"label1")
-Test.AddData([2,3,4,5,6],[1,2,3,4,5],"label2")
-Test.AddData([3,4,5,6,7],[1,2,3,4,5],"label3")
-Test.AddData([4,5,6,7,8],[1,2,3,4,5],"label4")
-Test.AddData([5,6,7,8,9],[1,2,3,4,5],"label5")
-Test.AddData([6,7,8,9,10],[1,2,3,4,5],"label6")
-Test.Plot()
-        
+
+
